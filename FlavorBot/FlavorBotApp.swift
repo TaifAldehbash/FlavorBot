5e3eba9a34ccf4404ec64c3b06a69c17499dc4d1
“TaifAldehbashgit config --global user.email “taifmaldehbash@gmail.com <77803844+TaifAldehbash@users.noreply.github.com>
2023-06-05 19:03:33 +0300

    Initial Commit

---
+++ FlavorBot/FlavorBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorBotApp.swift
+//  FlavorBot
+//
+//  Created by Taif Al.qahtani on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlavorBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
