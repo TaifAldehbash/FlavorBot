eac672ae522f9a70cd377339c50ea55e6f25c44f
“TaifAldehbashgit config --global user.email “taifmaldehbash@gmail.com <77803844+TaifAldehbash@users.noreply.github.com>
2023-06-11 16:05:43 +0300

    LaunchView ✅

---
+++ FlavorBot/Model/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  FlavorBot
+//
+//  Created by Taif Al.qahtani on 10/06/2023.
+//
+
+import Foundation
+import FirebaseCore
+import FirebaseAuth
+import FirebaseFirestore
+
+class FoodBotDB {
+    let db = Firestore.firestore()
+    let auth = Auth.auth()
+}
