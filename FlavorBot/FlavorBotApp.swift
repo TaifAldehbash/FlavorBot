f7a20c7ffa49ae4d088aa86b8a75ca3249bb9521
“TaifAldehbashgit config --global user.email “taifmaldehbash@gmail.com <77803844+TaifAldehbash@users.noreply.github.com>
2023-06-11 16:49:58 +0300

    Revert "LaunchView ✅"
    This reverts commit eac672ae522f9a70cd377339c50ea55e6f25c44f.

--- FlavorBot/FlavorBotApp.swift
+++ FlavorBot/FlavorBotApp.swift
@@ -6,23 +6,9 @@
 //
 
 import SwiftUI
-import FirebaseCore
-
-//Configure Firebase
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
-}
 
 @main
 struct FlavorBotApp: App {
-    
-    // register app delegate for Firebase setup
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
     var body: some Scene {
         WindowGroup {
             HomeView()
